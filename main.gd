dcc335a8b72aecb9178174d96b0704928adce1cd
persik <108504553+Alyona1619@users.noreply.github.com>
2024-10-23 19:21:08 +0800

    pipes, restart, fixes

--- main.gd
+++ main.gd
@@ -1,5 +1,7 @@
 extends Node
 
+@export var pipe_scene : PackedScene
+
 var game_running : bool
 var game_over : bool
 var scroll
@@ -14,14 +16,22 @@ const PIPE_RANGE : int = 200
 
 func _ready():
 	screen_size = get_window().size
+	ground_height = $Ground.get_node("Sprite2D").texture.get_height()
 	new_game()
 
 func new_game():
+	#reset variables
 	game_running = false
 	game_over = false
 	score = 0
 	scroll = 0
+	$ScoreLabel.text = "SCORE: " + str(score)
+	$GameOver.hide()
+	get_tree().call_group("pipes", "queue_free")
+	pipes.clear()
+	generate_pipes()
 	$Bird.reset()
+
 
 func _input(event):
 	if game_over == false:
@@ -38,12 +48,12 @@ func start_game():
 	game_running = true
 	$Bird.flying = true
 	$Bird.flap()
+	$PipeTimer.start()
 
 func check_top():
 	if $Bird.position.y < 0:
 		$Bird.falling = true
 		stop_game()
-		
 
 func stop_game():
 	$PipeTimer.stop()
@@ -53,9 +63,42 @@ func stop_game():
 	game_over = true
 	
 func _process(delta):
-		if game_running:
-			scroll += SCROLL_SPEED
+	if game_running:
+		scroll += SCROLL_SPEED
 		
-			if scroll >= screen_size.x:
-				scroll = 0
-			$Ground.position.x = -scroll
+		if scroll >= screen_size.x:
+			scroll = 0
+		
+		$Ground.position.x = -scroll
+		
+		for pipe in pipes:
+			pipe.position.x -= SCROLL_SPEED
+
+
+func _on_pipe_timer_timeout():
+	generate_pipes()
+	
+func generate_pipes():
+	var pipe = pipe_scene.instantiate()
+	pipe.position.x = screen_size.x + PIPE_DELAY
+	pipe.position.y = (screen_size.y - ground_height) / 2  + randi_range(-PIPE_RANGE, PIPE_RANGE)
+	pipe.hit.connect(bird_hit)
+	pipe.scored.connect(scored)
+	add_child(pipe)
+	pipes.append(pipe)
+	
+func bird_hit():
+	$Bird.falling = true
+	stop_game()
+	
+func scored():
+	score += 1
+	$ScoreLabel.text = "SCORE: " + str(score)
+	
+func _on_ground_hit():
+	$Bird.falling = false
+	stop_game()
+
+
+func _on_game_over_restart() -> void:
+	new_game()
